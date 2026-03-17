030eb6f06e1f4da5f262bdbed0a4e134823b7e40
Jun Aishima <jaishima@bnl.gov>
2026-03-17 13:12:11 -0400

    pre-commit fixes

--- consumer.py
+++ consumer.py
@@ -1,20 +1,22 @@
-from tiled.client.stream import Subscription
-from tiled.client import from_profile
-from prefect.deployments import run_deployment
 import argparse
 
+from prefect.deployments import run_deployment
+from tiled.client import from_uri
 
-arg_parser = argparse.ArgumentParser(description="Run a Prefect 2 workflow in response to a Kafka message.")
+arg_parser = argparse.ArgumentParser(
+    description="Run a Prefect 2 workflow in response to a Kafka message."
+)
 arg_parser.add_argument("endstation")
 arg_parser.add_argument("deployment_name")
-args = get_arg_parser().parse_args()
+args = arg_parser.parse_args()
 
 
 def on_child_metadata_updated(update):
-    print(f"Metadata updated, stop_doc: {update.metadata.get('stop')}")
-    print(f"stop document:\n{pformat(doc)}")
-    print(f"run deployment {args.deployment_name}")
-    doc = update.metadata.get('stop')
+    print(
+        f"Metadata updated, stop_doc: {update.metadata.get('stop')}\
+          - run deployment {args.deployment_name}"
+    )
+    doc = update.metadata.get("stop")
     if doc:
         run_deployment(
             name=args.deployment_name,
@@ -22,16 +24,19 @@ def on_child_metadata_updated(update):
             timeout=0,
         )
     else:
-        print(update.metadata['name'])
+        print(update.metadata["name"])
         pass
 
+
 def message_to_workflow():
-    client = from_uri('https://tiled.nsls2.bnl.gov')
-    pt = client[f'{args.endstation}/migration']
+    client = from_uri("https://tiled.nsls2.bnl.gov")
+    pt = client[f"{args.endstation}/migration"]
     sub = pt.subscribe()
     sub.child_metadata_updated.add_callback(on_child_metadata_updated)
-    print(f"Listening for updates on {args.endstation} for deployment {args.deployment_name}.")
+    print(f"Listening on \
+            {args.endstation} for deployment {args.deployment_name}.")
     sub.start()  # block
+
 
 if __name__ == "__main__":
     message_to_workflow()
